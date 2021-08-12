6c5f464905618463ece2cddfd6e26ea53e189f15
Julian Voss <julian.voss@hs-osnabrueck.de>
2021-08-12 17:24:35 +0200

    Statusbar transparent

--- app/src/main/java/de/prog3/ackerschlagkartei/activities/SignInActivity.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/activities/SignInActivity.java
@@ -1,11 +1,16 @@
 package de.prog3.ackerschlagkartei.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import de.prog3.ackerschlagkartei.R;
@@ -26,9 +31,12 @@ public class SignInActivity extends AppCompatActivity {
         this.passwordEditText = findViewById(R.id.etPassword);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         this.setContentView(R.layout.activity_sign_in);
         this.instantiateUI();
     }
