e82324192030c568f6b3f3b4fc41974a8f886246
Patrick Felschen <patrick@felschen.de>
2021-08-13 15:33:00 +0200

    temp button for details

--- app/src/main/java/de/prog3/ackerschlagkartei/fragments/FieldsOverviewListFragment.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/fragments/FieldsOverviewListFragment.java
@@ -1,13 +1,16 @@
 package de.prog3.ackerschlagkartei.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import de.prog3.ackerschlagkartei.R;
+import de.prog3.ackerschlagkartei.activities.FieldDetailsActivity;
 
 public class FieldsOverviewListFragment extends Fragment {
 
@@ -26,6 +29,16 @@ public class FieldsOverviewListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fields_overview_list, container, false);
+
+        Button btnOpenDetails = view.findViewById(R.id.btn_open_details);
+
+        btnOpenDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), FieldDetailsActivity.class);
+                startActivity(i);
+            }
+        });
 
         return view;
     }
