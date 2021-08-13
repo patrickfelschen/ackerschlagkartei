49fdda5e3847bf5d195a1f14197b308b79b4d724
Julian Voss <julian.voss@hs-osnabrueck.de>
2021-08-13 11:20:30 +0200

    SignUpActivity

--- app/src/main/java/de/prog3/ackerschlagkartei/activities/SignUpActivity.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/activities/SignUpActivity.java
@@ -10,29 +10,32 @@ import android.widget.EditText;
 
 import de.prog3.ackerschlagkartei.R;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
-    private EditText emailEditText;
-    private Button forgotPasswordButton;
+    private EditText signUpMailEditText;
+    private EditText signUpPasswordEditText;
+    private EditText signUpPasswordConfirmEditText;
+    private Button signUpButton;
     private Button returnToSignInButton;
 
     private void instantiateUI() {
-        this.emailEditText = (EditText) findViewById(R.id.etForgotPasswordMail);
-        this.forgotPasswordButton = (Button) findViewById(R.id.btnForgotPassword);
+        this.signUpMailEditText = (EditText) findViewById(R.id.etSignUpMail);
+        this.signUpPasswordEditText = (EditText) findViewById(R.id.etSignUpPassword);
+        this.signUpPasswordConfirmEditText = (EditText) findViewById(R.id.etSignUpPasswordConfirm);
+        this.signUpButton = (Button) findViewById(R.id.btnSignUp);
         this.returnToSignInButton = (Button) findViewById(R.id.btnReturnToSignIn);
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
-        setContentView(R.layout.activity_forgot_password);
-        instantiateUI();
+        setContentView(R.layout.activity_sign_up);
+        this.instantiateUI();
     }
 
-    public void forgotPasswordClick(View v) {
+    private void signUpButtonClick(View v) {
 
     }
 
