82208da08c18a3f4c93cb7ba947590f452e066fe
Patrick Felschen <patrick@felschen.de>
2021-08-13 14:30:49 +0200

    Fieldmodel added

--- app/src/main/java/de/prog3/ackerschlagkartei/models/FieldModel.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/models/FieldModel.java
@@ -1,4 +1,17 @@
 package de.prog3.ackerschlagkartei.models;
 
 public class FieldModel {
+    public String uid;
+    public String name;
+    public String desc;
+
+    public FieldModel() {
+
+    }
+
+    public FieldModel(String uid, String name, String desc) {
+        this.uid = uid;
+        this.name = name;
+        this.desc = desc;
+    }
 }
