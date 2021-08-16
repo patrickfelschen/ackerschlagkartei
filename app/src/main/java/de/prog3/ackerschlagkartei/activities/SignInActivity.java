72c46027d2b2545cdefeea5658f0b507ac1b9f9b
Patrick Felschen <patrick@felschen.de>
2021-08-16 19:05:26 +0200

    sign in add

--- app/src/main/java/de/prog3/ackerschlagkartei/activities/SignInActivity.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/activities/SignInActivity.java
@@ -3,36 +3,31 @@ package de.prog3.ackerschlagkartei.activities;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import de.prog3.ackerschlagkartei.R;
-
-import static android.content.ContentValues.TAG;
 
 public class SignInActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
 
-    private Button signInButton;
-    private Button openSignUpButton;
-    private Button openPasswordResetButton;
-    private EditText emailEditText;
-    private EditText passwordEditText;
-
-    private void instantiateUI() {
-        this.signInButton = findViewById(R.id.btnSignIn);
-        this.openSignUpButton = findViewById(R.id.btnOpenSignUp);
-        this.openPasswordResetButton = findViewById(R.id.btnOpenPasswordReset);
-        this.emailEditText = findViewById(R.id.etEmail);
-        this.passwordEditText = findViewById(R.id.etPassword);
-    }
+    private Button btnSignIn;
+    private Button btnOpenSignUp;
+    private Button btnOpenPasswordReset;
+    private EditText etSignInEmail;
+    private EditText etSignInPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,26 +38,66 @@ public class SignInActivity extends AppCompatActivity {
         this.firebaseAuth = FirebaseAuth.getInstance();
 
         this.setContentView(R.layout.activity_sign_in);
-        this.instantiateUI();
+
+        this.btnSignIn = findViewById(R.id.btn_sign_in);
+        this.btnOpenSignUp = findViewById(R.id.btn_open_sign_up);
+        this.btnOpenPasswordReset = findViewById(R.id.btn_open_password_reset);
+        this.etSignInEmail = findViewById(R.id.et_sign_in_email);
+        this.etSignInPassword = findViewById(R.id.et_sign_in_password);
+
+        this.btnSignIn.setOnClickListener(btnSignInClick);
+        this.btnOpenSignUp.setOnClickListener(btnOpenSignUpClick);
+        this.btnOpenPasswordReset.setOnClickListener(btnOpenPasswordResetClick);
     }
 
-    public void signInButtonClick(View v) {
-        Log.d(TAG, "signInButtonClick");
-        String email = emailEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
+    private final View.OnClickListener btnSignInClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            signInFirebaseUser();
+        }
+    };
 
-        Intent intent = new Intent(this, FieldsOverviewActivity.class);
-        this.startActivity(intent);
+    private final View.OnClickListener btnOpenSignUpClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+        }
+    };
+
+    private final View.OnClickListener btnOpenPasswordResetClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(SignInActivity.this, ForgotPasswordActivity.class));
+        }
+    };
+
+    private void signInFirebaseUser() {
+        String email = etSignInEmail.getText().toString().trim();
+        String password = etSignInPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty((email))) {
+            etSignInEmail.setError("Email cannot be empty");
+            etSignInEmail.requestFocus();
+        } else if (TextUtils.getTrimmedLength(password) < 8) {
+            etSignInPassword.setError("Password too short - must be at least 8 characters");
+            etSignInPassword.requestFocus();
+        } else {
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(signInOnCompleteListener());
+        }
     }
 
-    public void openSignUpButtonClick(View v) {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        this.startActivity(intent);
-    }
-
-    public void openPasswordResetClick(View v) {
-        Intent intent = new Intent(this, ForgotPasswordActivity.class);
-        this.startActivity(intent);
+    private OnCompleteListener<AuthResult> signInOnCompleteListener() {
+        return new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignInActivity.this, "Sign in successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SignInActivity.this, FieldsOverviewActivity.class));
+                } else {
+                    Toast.makeText(SignInActivity.this, "Sign in failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
     }
 
 }
--- app/src/main/java/de/prog3/ackerschlagkartei/activities/SignUpActivity.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/activities/SignUpActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,26 +19,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import de.prog3.ackerschlagkartei.R;
 
-import static android.content.ContentValues.TAG;
-
 public class SignUpActivity extends AppCompatActivity {
 
-    private final View.OnClickListener returnToSignInClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            finishActivity();
-        }
-    };
+
     private FirebaseAuth firebaseAuth;
     private EditText etSignUpEmail;
     private EditText etSignUpPassword;
     private EditText etSignUpPasswordConfirm;
-    private final View.OnClickListener signUpButtonClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            createFirebaseUser();
-        }
-    };
     private Button btnSignUp;
     private Button btnReturnToSignIn;
 
@@ -60,8 +46,21 @@ public class SignUpActivity extends AppCompatActivity {
 
         this.btnSignUp.setOnClickListener(signUpButtonClick);
         this.btnReturnToSignIn.setOnClickListener(returnToSignInClick);
-
     }
+
+    private final View.OnClickListener returnToSignInClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finishActivity();
+        }
+    };
+
+    private final View.OnClickListener signUpButtonClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            createFirebaseUser();
+        }
+    };
 
     private void createFirebaseUser() {
         String email = etSignUpEmail.getText().toString().trim();
@@ -78,7 +77,6 @@ public class SignUpActivity extends AppCompatActivity {
             etSignUpPasswordConfirm.setError("Password doesn't match");
             etSignUpPasswordConfirm.requestFocus();
         } else {
-            Log.d(TAG, "Email: " + email + "Password: " + password);
             firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(signUpOnCompleteListener());
         }
 
