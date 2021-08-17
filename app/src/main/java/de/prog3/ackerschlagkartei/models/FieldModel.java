226009b5893f69ed60347dcb545d2b9c8f81e642
Patrick Felschen <patrick@felschen.de>
2021-08-17 10:55:16 +0200

    fieldModel for firestore

--- app/src/main/java/de/prog3/ackerschlagkartei/models/FieldModel.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/models/FieldModel.java
@@ -1,17 +1,50 @@
 package de.prog3.ackerschlagkartei.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FieldModel {
-    public String uid;
-    public String name;
-    public String desc;
+    private String uid;
+    private CultivationModel cultivation;
+    private GroundModel ground;
+    private InfoModel info;
+    private List<AccountModel> actions;
+    private List<ImageModel> images;
 
     public FieldModel() {
-
+        // Firestore
     }
 
-    public FieldModel(String uid, String name, String desc) {
-        this.uid = uid;
-        this.name = name;
-        this.desc = desc;
+    public FieldModel(CultivationModel cultivation, GroundModel ground, InfoModel info) {
+        this.uid = "";
+        this.cultivation = cultivation;
+        this.ground = ground;
+        this.info = info;
+        this.actions = new ArrayList<>();
+        this.images = new ArrayList<>();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public CultivationModel getCultivation() {
+        return cultivation;
+    }
+
+    public GroundModel getGround() {
+        return ground;
+    }
+
+    public InfoModel getInfo() {
+        return info;
+    }
+
+    public List<AccountModel> getActions() {
+        return actions;
+    }
+
+    public List<ImageModel> getImages() {
+        return images;
     }
 }
