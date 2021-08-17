88ebcc52c9ccf2a4a06d3adb13875d0df2378b9c
Patrick Felschen <patrick@felschen.de>
2021-08-17 11:09:41 +0200

    models for firestore

--- app/src/main/java/de/prog3/ackerschlagkartei/models/GroundModel.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/models/GroundModel.java
@@ -1,4 +1,52 @@
 package de.prog3.ackerschlagkartei.models;
 
 public class GroundModel {
+    private String bkr;
+    private String groundType;
+    private String humus;
+    private float magnesium;
+    private float phValue;
+    private float phosphorus;
+    private float potassium;
+
+    public GroundModel() {
+    }
+
+    public GroundModel(String bkr, String groundType, String humus, float magnesium, float phValue, float phosphorus, float potassium) {
+        this.bkr = bkr;
+        this.groundType = groundType;
+        this.humus = humus;
+        this.magnesium = magnesium;
+        this.phValue = phValue;
+        this.phosphorus = phosphorus;
+        this.potassium = potassium;
+    }
+
+    public String getBkr() {
+        return bkr;
+    }
+
+    public String getGroundType() {
+        return groundType;
+    }
+
+    public String getHumus() {
+        return humus;
+    }
+
+    public float getMagnesium() {
+        return magnesium;
+    }
+
+    public float getPhValue() {
+        return phValue;
+    }
+
+    public float getPhosphorus() {
+        return phosphorus;
+    }
+
+    public float getPotassium() {
+        return potassium;
+    }
 }
--- app/src/main/java/de/prog3/ackerschlagkartei/models/InfoModel.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/models/InfoModel.java
@@ -1,4 +1,48 @@
 package de.prog3.ackerschlagkartei.models;
 
+import com.google.firebase.firestore.GeoPoint;
+
 public class InfoModel {
+    private float area;
+    private String description;
+    private boolean phosphateSensitiveArea;
+    private boolean redArea;
+    private boolean waterProtectionArea;
+    private GeoPoint[] positions;
+
+    public InfoModel() {
+    }
+
+    public InfoModel(float area, String description, boolean phosphateSensitiveArea, boolean redArea, boolean waterProtectionArea, GeoPoint[] positions) {
+        this.area = area;
+        this.description = description;
+        this.phosphateSensitiveArea = phosphateSensitiveArea;
+        this.redArea = redArea;
+        this.waterProtectionArea = waterProtectionArea;
+        this.positions = positions;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isPhosphateSensitiveArea() {
+        return phosphateSensitiveArea;
+    }
+
+    public boolean isRedArea() {
+        return redArea;
+    }
+
+    public boolean isWaterProtectionArea() {
+        return waterProtectionArea;
+    }
+
+    public GeoPoint[] getPositions() {
+        return positions;
+    }
 }
