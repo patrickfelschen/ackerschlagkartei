8e46854643a4348bc5479adf09d9aa579efb826a
Patrick Felschen <patrick@felschen.de>
2021-08-19 12:02:56 +0200

    new list item

--- app/src/main/java/de/prog3/ackerschlagkartei/activities/FieldDetailsActivity.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/activities/FieldDetailsActivity.java
@@ -3,10 +3,12 @@ package de.prog3.ackerschlagkartei.activities;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -14,8 +16,11 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import de.prog3.ackerschlagkartei.R;
+import de.prog3.ackerschlagkartei.viewmodels.FieldsOverviewViewModel;
 
 public class FieldDetailsActivity extends AppCompatActivity {
+
+    private FieldsOverviewViewModel fieldViewModel;
 
     private Toolbar fieldDetailsToolbar;
 
@@ -27,6 +32,8 @@ public class FieldDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field_details);
 
+        this.fieldViewModel = new ViewModelProvider(this).get(FieldsOverviewViewModel.class);
+
         this.fieldDetailsToolbar = findViewById(R.id.field_details_toolbar);
         this.bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         this.navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -34,6 +41,8 @@ public class FieldDetailsActivity extends AppCompatActivity {
         setSupportActionBar(fieldDetailsToolbar);
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        Toast.makeText(this, fieldViewModel.getSelectedField().getInfo().getDescription(), Toast.LENGTH_SHORT).show();
 
     }
 
--- app/src/main/java/de/prog3/ackerschlagkartei/viewmodels/FieldsOverviewViewModel.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/viewmodels/FieldsOverviewViewModel.java
@@ -20,6 +20,8 @@ public class FieldsOverviewViewModel extends AndroidViewModel {
     private final MutableLiveData<List<FieldModel>> fieldMutableLiveData;
     private final FirestoreRepository fieldsOverviewListRepository;
 
+    private FieldModel selectedField;
+
     public FieldsOverviewViewModel(Application application) {
         super(application);
 
@@ -40,5 +42,13 @@ public class FieldsOverviewViewModel extends AndroidViewModel {
 
     public MutableLiveData<List<FieldModel>> getLiveFieldData() {
         return this.fieldMutableLiveData;
+    }
+
+    public FieldModel getSelectedField() {
+        return selectedField;
+    }
+
+    public void setSelectedField(FieldModel selectedField) {
+        this.selectedField = selectedField;
     }
 }
