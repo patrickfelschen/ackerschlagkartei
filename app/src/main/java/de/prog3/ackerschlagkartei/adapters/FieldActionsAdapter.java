c62d5c6aa1fe227acfac266eef0e40f213800608
Julian Voss <julian.voss@hs-osnabrueck.de>
2021-08-23 12:57:03 +0200

    FieldActions List

--- app/src/main/java/de/prog3/ackerschlagkartei/adapters/FieldActionsAdapter.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/adapters/FieldActionsAdapter.java
@@ -1,95 +1,57 @@
 package de.prog3.ackerschlagkartei.adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.HashMap;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import de.prog3.ackerschlagkartei.R;
+import de.prog3.ackerschlagkartei.activities.FieldDetailsActivity;
+import de.prog3.ackerschlagkartei.models.FieldModel;
 
-public class FieldActionsAdapter extends BaseExpandableListAdapter {
+public class FieldActionsAdapter extends ArrayAdapter {
+    List objects;
+    Integer[] images;
+    Activity context;
 
-    Context context;
-    List<String> listGroup;
-    HashMap<String, List<String>> listItem;
-
-    public FieldActionsAdapter(Context context, List<String> listGroup, HashMap<String, List<String>> listItem) {
+    public FieldActionsAdapter(@NonNull Activity context, List objects, Integer[] images) {
+        super(context, R.layout.list_item, objects);
         this.context = context;
-        this.listGroup = listGroup;
-        this.listItem = listItem;
+        this.objects = objects;
+        this.images = images;
     }
 
+    @NonNull
     @Override
-    public int getGroupCount() {
-        return listGroup.size();
-    }
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
 
-    @Override
-    public int getChildrenCount(int groupPosition) {
-        return this.listItem.get(this.listGroup.get(groupPosition)).size();
-    }
-
-    @Override
-    public Object getGroup(int groupPosition) {
-        return this.listGroup.get(groupPosition);
-    }
-
-    @Override
-    public Object getChild(int groupPosition, int childPosition) {
-        return this.listItem.get(this.listGroup.get(groupPosition)
-        )                           .get(childPosition);
-    }
-
-    @Override
-    public long getGroupId(int groupPosition) {
-        return groupPosition;
-    }
-
-    @Override
-    public long getChildId(int groupPosition, int childPosition) {
-        return childPosition;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
-
-    @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        String group = (String) getGroup(groupPosition);
+        LayoutInflater inflater = context.getLayoutInflater();
         if(convertView == null) {
-            LayoutInflater layoutInflater = (LayoutInflater) this.context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = layoutInflater.inflate(R.layout.list_group, null);
+            row = inflater.inflate(R.layout.list_item, null, true);
         }
 
-        TextView textView = convertView.findViewById(R.id.list_parent);
-        textView.setText(group);
-        return convertView;
-    }
+        TextView labelListItem = row.findViewById(R.id.list_child);
+        ImageView iconListItem = row.findViewById(R.id.list_item_icon);
 
-    @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        String child = (String) getChild(groupPosition,childPosition);
-        if(convertView == null) {
-            LayoutInflater layoutInflater = (LayoutInflater) this.context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = layoutInflater.inflate(R.layout.list_item, null);
+        labelListItem.setText(objects.get(position).toString());
+        if(images != null) {
+            iconListItem.setImageResource(images[position]);
         }
 
-        TextView textView = convertView.findViewById(R.id.list_child);
-        textView.setText(child);
-        return convertView;
-    }
-
-    @Override
-    public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return true;
+        return row;
     }
 }
