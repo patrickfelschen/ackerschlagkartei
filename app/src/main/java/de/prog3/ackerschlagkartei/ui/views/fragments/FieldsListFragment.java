4cde9d24afd427711639185719c80b717e7ebaaf
Julian Voss <julian.voss@hs-osnabrueck.de>
2021-08-25 11:17:00 +0200

    Show Fields on FieldsListFragment

--- app/src/main/java/de/prog3/ackerschlagkartei/ui/views/fragments/FieldsListFragment.java
+++ app/src/main/java/de/prog3/ackerschlagkartei/ui/views/fragments/FieldsListFragment.java
@@ -11,16 +11,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import de.prog3.ackerschlagkartei.R;
+import de.prog3.ackerschlagkartei.data.models.FieldModel;
+import de.prog3.ackerschlagkartei.ui.adapters.FieldsListAdapter;
 import de.prog3.ackerschlagkartei.ui.viewmodels.FieldsViewModel;
 
 public class FieldsListFragment extends Fragment {
     private FieldsViewModel fieldsViewModel;
     private NavController navController;
+    private RecyclerView fieldList;
+    FieldsListAdapter fieldsListAdapter;
 
     public FieldsListFragment() { }
 
@@ -41,8 +50,24 @@ public class FieldsListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        this.fieldsViewModel = new ViewModelProvider(this).get(FieldsViewModel.class);
+        this.fieldList = view.findViewById(R.id.rv_field_list);
+        this.fieldList.setLayoutManager(new LinearLayoutManager(getContext()));
+        this.fieldList.setHasFixedSize(true);
+
         this.navController = Navigation.findNavController(view);
+
+        this.fieldsListAdapter = new FieldsListAdapter();
+        this.fieldList.setAdapter(fieldsListAdapter);
+
+        this.fieldsViewModel = new ViewModelProvider(this).get(FieldsViewModel.class);
+        this.fieldsViewModel.getLiveFieldData().observe(requireActivity(), new Observer<List<FieldModel>>() {
+            @Override
+            public void onChanged(List<FieldModel> fieldModels) {
+                fieldsListAdapter.setFieldModelList(fieldModels);
+                fieldsListAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 
     @Override
